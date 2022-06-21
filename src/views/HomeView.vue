9bf5657a14b16ad3138b468d3e213925e8be721d
mainphase1 <humantorchf@live.fr>
2022-06-21 17:26:20 +0200

    ajoutbootstrap

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,47 @@
 <template>
   <div class="home">
+  
+  
+   <p>Using stand-alone:<p/>
+    <div class="mb-4">
+      <b-avatar></b-avatar>
+      <b-avatar variant="primary" text="BV"></b-avatar>
+      <b-avatar variant="info" src="https://placekitten.com/300/300"></b-avatar>
+      <b-avatar variant="success" icon="people-fill"></b-avatar>
+    </div>
+    <p>Using in components (list group) example:<p/>
+    <b-list-group style="max-width: 300px;">
+      <b-list-group-item class="d-flex align-items-center">
+        <b-avatar class="mr-3"></b-avatar>
+        <span class="mr-auto">J. Circlehead</span>
+        <b-badge>5</b-badge>
+      </b-list-group-item>
+      <b-list-group-item class="d-flex align-items-center">
+        <b-avatar variant="primary" text="BV" class="mr-3"></b-avatar>
+        <span class="mr-auto">BootstrapVue</span>
+        <b-badge>12</b-badge>
+      </b-list-group-item>
+      <b-list-group-item class="d-flex align-items-center">
+        <b-avatar variant="info" src="https://placekitten.com/300/300" class="mr-3"></b-avatar>
+        <span class="mr-auto">Super Kitty</span>
+        <b-badge>9</b-badge>
+      </b-list-group-item>
+      <b-list-group-item class="d-flex align-items-center">
+        <b-avatar variant="success" icon="people-fill" class="mr-3"></b-avatar>
+        <span class="mr-auto">ACME group</span>
+        <b-badge>7</b-badge>
+      </b-list-group-item>
+    </b-list-group>
+  
+   <b-button v-b-modal.modal-1 variant="warning">Launch demo modal</b-button>
+
+  <b-modal id="modal-1" title="Creatic">
+    <p class="my-4">salut toi!</p>
+  </b-modal>
+  
+  
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg=" monde"/>
+    <HelloWorld msg=" hello everyone"/>
   </div>
 </template>
 
